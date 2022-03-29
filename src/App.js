10870cfbadce4ff051d2050648769e3945d27ce6
John Zarraga <jlzd1994@gmail.com>
2022-03-28 22:31:03 -0300

    Prueba de lat y long en app

--- src/App.js
+++ src/App.js
@@ -42,13 +42,13 @@ const App = () => {
   const teeeest = () => {
 
       //Si el valor que recibe es true entonces enviamos el correo
-      // navigator.geolocation.getCurrentPosition(function (position) {
-      //   console.log("Latitude is :", position.coords.latitude);
+      navigator.geolocation.getCurrentPosition(function (position) {
+        console.log("Latitude is :", position.coords.latitude);
 
-      //   alert("LAT: "+position.coords.latitude);
+        alert("LAT: "+position.coords.latitude);
 
-      //   console.log("Longitude is :", position.coords.longitude);
-      // })
+        console.log("Longitude is :", position.coords.longitude);
+      })
   }
 
   const btnDistance = () =>{
